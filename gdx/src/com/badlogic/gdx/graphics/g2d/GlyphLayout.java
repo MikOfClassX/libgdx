52ef79b811f5e4370f7bc183e33307dfe919d04d
Dar <dar@classx.it>
2025-07-21 10:08:52 +0200

    fix: adapt to new api

--- gdx/src/com/badlogic/gdx/graphics/g2d/GlyphLayout.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/GlyphLayout.java
@@ -45,7 +45,7 @@ import com.badlogic.gdx.utils.Pools;
  * @author Alexander Dorokhov
  * @author Thomas Creutzenberg */
 public class GlyphLayout implements Poolable {
-	private final Pool<GlyphRun> glyphRunPool = Pools.get(GlyphRun::new);
+	private final Pool<GlyphRun> glyphRunPool = Pools.get(GlyphRun.class);
 	private final IntArray colorStack = new IntArray(4);
 	static private final float epsilon = 0.0001f;
 
